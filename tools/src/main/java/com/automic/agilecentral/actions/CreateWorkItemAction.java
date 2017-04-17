5c528da54d215ddbd7ec14871c22a465b14c92d6
Anurag Upadhyay <extanu@automic.com>
2017-04-17 14:11:01 +0530

    PMARA-5004<PMARA-5031>:Delete file ae post condition & handle costom field java and AE both

--- tools/src/main/java/com/automic/agilecentral/actions/CreateWorkItemAction.java
+++ tools/src/main/java/com/automic/agilecentral/actions/CreateWorkItemAction.java
@@ -3,12 +3,18 @@
  */
 package com.automic.agilecentral.actions;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.automic.agilecentral.constants.Constants;
 import com.automic.agilecentral.exception.AutomicException;
@@ -32,7 +38,7 @@ public class CreateWorkItemAction extends AbstractHttpAction {
 	private String workItemType;
 	private String description;
 	private String scheduleState;
-	private String customFields;
+	private List<String> customFields = Collections.emptyList();
 
 	public CreateWorkItemAction() {
 
@@ -42,7 +48,7 @@ public class CreateWorkItemAction extends AbstractHttpAction {
 		addOption("projectname", false, "Project Name where work item needs to be created");
 		addOption("description", false, "Desciption of work item");
 		addOption("schedulestate", false, "Schedule state of work");
-		addOption("customfields", false, "Custom fields");
+		addOption("filepath", false, "Custom fields file path");
 
 	}
 
@@ -51,15 +57,15 @@ public class CreateWorkItemAction extends AbstractHttpAction {
 
 		JsonObject newObj = new JsonObject();
 		checkandPrepareInputs(newObj);
-
-		if (CommonUtil.checkNotEmpty(customFields)) {
+		
+		if (!customFields.isEmpty()) {
 			addCustomfileds(newObj);
 		}
-
+		ConsoleWriter.writeln("Request Json Object: "+newObj);
 		CreateRequest createRequest = new CreateRequest(workItemType, newObj);
 		try {
 			CreateResponse createResponse = rallyRestTarget.create(createRequest);
-
+			ConsoleWriter.writeln("Response Json Object: "+createResponse.getObject());
 			if (createResponse.wasSuccessful()) {
 				ConsoleWriter.writeln(
 						"UC4RB_AC_WORK_ITEM_ID ::=" + createResponse.getObject().get("FormattedID").getAsString());
@@ -92,10 +98,20 @@ public class CreateWorkItemAction extends AbstractHttpAction {
 
 		description = getOptionValue("description");
 
-		customFields = getOptionValue("customfields");
-
-		newObj.addProperty("Description", description);
-		newObj.addProperty("ScheduleState", scheduleState);
+		//customFields = getOptionValue("customfields");
+		String temp = getOptionValue("filepath");
+		if (CommonUtil.checkNotEmpty(temp)) {
+			File file = new File(temp);
+            AgileCentralValidator.checkFileExists(file);
+            customFields = getCustomFields(temp);
+		}		
+		
+		if (CommonUtil.checkNotEmpty(description)) {
+			newObj.addProperty("Description", description);
+		}
+		if (CommonUtil.checkNotEmpty(scheduleState)) {
+			newObj.addProperty("ScheduleState", scheduleState);
+		}	
 
 		Map<String, String> queryFilter;
 		List<String> fetch;
@@ -137,16 +153,36 @@ public class CreateWorkItemAction extends AbstractHttpAction {
 	 */
 	private void addCustomfileds(JsonObject newObj) throws AutomicException {
 		try {
-			String regex = System.getProperty("line.separator");
-			String[] listOfFields = customFields.split(regex);
-			for (String fields : listOfFields) {
+			//String regex = System.getProperty("line.separator");
+			//String[] listOfFields = customFields.split(regex);
+			for (String fields : customFields) {
 				String[] field = fields.split("=");
-				newObj.addProperty(field[0], field[1]);
+				if(field.length == 2){
+					Arrays.stream(field).map(String::trim).toArray(unused -> field);//Trim original array element
+					newObj.addProperty(field[0], field[1]);
+				}
 			}
 		} catch (Exception e) {
 			throw new AutomicException(
 					"Error in the given custom fields ,please provide the valid input e.g Key1=Val1 \\n Key2=Val2");
 		}
 	}
+	
+	private List<String> getCustomFields(String fileName){
+		List<String> list = new ArrayList<>();
+
+		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+			list = stream
+					.filter(s -> !s.isEmpty())// trim them and filter out all empty lines
+					.map(line ->  line.trim())
+					.collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
+	
 
 }
